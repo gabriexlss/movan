15d123b97a7011d551a86ff7f7a3a4d948a4d40c
Kdura-ne <wilsonrobertocaetanoneto@gmail.com>
2026-06-01 11:01:02 -0300

    Adição inicial do footer

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,19 +3,15 @@ import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import './App.css'
 
 import Header from './components/header'
+import Footer from './components/footer'
 
 function App() {
 
   return (
-    <>
     <BrowserRouter>
-      <Routes>
-        <Route path="*" element={<Header />} />
-        {/* <Route path="*" element={<DivElemento />} />
-        <Route path="*" element={<Footer />} /> */}
-      </Routes>
+        <Header />
+        <Footer />
     </BrowserRouter>
-    </>
   )
 }
 
---
+++ client/src/components/footer.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import './footer.css';
+
+const footer = () => {
+
+    const navFooter = [
+        {href: '/', label: 'home'},
+        {href: '/', label: 'financeiro'},
+        {href: '/', label: 'rota'},
+        {href: '/', label: 'perfil'}
+    ];
+
+    return (
+        <div className="footer">
+            
+        </div>
+    )
+
+}
+
+export default footer
