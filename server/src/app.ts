6327ab6fcc84daab686f2ea184dbb6fd02a3be01
gabriexlss <gabrielsasukeclash@gmail.com>
2026-06-17 22:07:57 -0300

    feat: setup do cors - criação do cors, permitindo os metodos GET, POST, PUT, DELETE e PATCH - as origens, ou seja o que ele permite acessar, ta tudo vindo do arquivo .env, e tem que ser um array JSON com aspas duplas, exemplo: ["http://localhost:5173", "http://localhost:5174"]

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,7 +1,27 @@
 import express, { Application } from 'express';
+import cors from 'cors';
 import mainRoutes from './mainRoutes.js';
 
 const app: Application = express();
+
+// Processa as origens do CORS a partir do .env, coloquei um try pra tentar previnir erros
+const allowedOrigins = (() => {
+  const originEnv = process.env['CORS_ORIGEM'];
+  if (!originEnv) return [];
+  try {
+    return JSON.parse(originEnv);
+  } catch {
+    // Caso não seja um JSON válido (ex: uma única string), retorna a string diretamente
+    return originEnv;
+  }
+});
+
+// Habilita o básico de CORS permitindo as origens configuradas
+app.use(cors({
+  origin: allowedOrigins,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+  credentials: true
+}));
 
 // Necessário para o servidor conseguir ler o corpo das requisições (req.body) em JSON
 app.use(express.json());
