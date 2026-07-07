a697c45eb21614c1887036f2b5502ff0d87ff87d
gabriexlss <gabrielsasukeclash@gmail.com>
2026-07-07 12:18:29 -0300

    bugfix: codigo de teste em prod

--- server/src/controllers/motorista.controller.ts
+++ server/src/controllers/motorista.controller.ts
@@ -29,10 +29,9 @@ export const controllerMotorista = {
             const valores = [nome, cnpj, email, senhahash]
             
             // finalmente pega os dados e faz o insert no banco de dados
+            await database.query(query, valores)
             return res.status(201).json({
-                msg: "Motorista criado com sucesso!",
-                query,
-                valores
+                msg: "Motorista criado com sucesso!"
             })
         }catch(erro){
             // tratamento de dados, basicamente da uma mensagem no console do servidor com o erro onde nenhum usuario ve e pro usuario manda uma msg bonitinha
