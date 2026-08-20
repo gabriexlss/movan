417a4be910aac7f963bd6bca7cee7eef44044d5b
gabriexlss <gabrielsasukeclash@gmail.com>
2026-08-20 18:16:47 -0300

    fix: arrumando IDs para usarem .int e email pra usar .email no schema do zod

--- server/src/models/responsavel.model.ts
+++ server/src/models/responsavel.model.ts
@@ -2,13 +2,13 @@ import { z } from "zod";
 
 
 const ResponsavelSchema = z.object({
-    id: z.number("id tem que ser um número").positive("id tem que ser um numero positivo"),
+    id: z.number("id tem que ser um número").positive("id tem que ser um numero positivo").int(),
     cpf: z.string("cpf tem que ser um string").length(11, "cpf tem que ter exatamente 11 caracteres"),
     nome: z.string("nome tem que ser um string").min(5, "nome completo tem que ter no minimo 5 caracteres").max(200, "nome completo tem que ter no maximo 200 caracteres"),
     endereco: z.string("endereço tem que ser um string").min(10, "endereço tem que ter no minimo 10 caracteres").max(255, "endereço tem que ter no maximo 255 caracteres"),
     tel: z.string("telefone tem que ser um string").length(11, "telefone tem que ter 11 caracteres"),
-    email: z.string("email tem que ser um string").min(11, "email tem que ter no minimo 11 caracteres").max(150, "email tem que ter no maximo 150 caracteres"),
-    motorista_id: z.number("id tem que ser um numero").positive("id tem que ser um numero positivo")
+    email: z.string("email tem que ser um string").min(11, "email tem que ter no minimo 11 caracteres").max(150, "email tem que ter no maximo 150 caracteres").email("Tem que ser um email valido"),
+    motorista_id: z.number("id tem que ser um numero").positive("id tem que ser um numero positivo").int()
 })
 export const CriarResponsavelSchema = ResponsavelSchema.pick({
     cpf: true,
@@ -29,7 +29,7 @@ export const IdResponsavelSchema = ResponsavelSchema.pick({
     id: true
 })
 export const IdResponsavelParamsSchema = z.object({
-    id: z.coerce.number().positive().optional()
+    id: z.coerce.number().positive().int().optional()
 })
 
 export type IdResponsavelOpcional = z.infer<typeof IdResponsavelParamsSchema>
