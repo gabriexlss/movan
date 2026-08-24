2db230f8a5034b977f3b2f5eeb96a193f394415a
gabriexlss <gabrielsasukeclash@gmail.com>
2026-08-23 22:11:37 -0300

    feat: endpoint para editar escola criado

--- server/src/controllers/escola.controller.ts
+++ server/src/controllers/escola.controller.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from "express";
-import { CriarEscolaSchema } from "../models/escola.model.js";
+import { CriarEscolaSchema, EditarEscolaSchema } from "../models/escola.model.js";
 import { database } from "../db/postgre.js";
 
 export const controllerEscola = {
@@ -21,7 +21,7 @@ export const controllerEscola = {
         const { nome, endereco, tel, hora_abertura, hora_fechamento, latitude, longitude } = dadosBrutos.data
 
         // realizando a operação
-        try{
+        try {
             const query = `INSERT INTO escola 
             (nome, endereco, tel, hora_abertura, hora_fechamento, latitude, longitude, motorista_id) 
             VALUES ($1, $2, $3, $4, $5, $6, $7, $8)
@@ -34,8 +34,91 @@ export const controllerEscola = {
                 msg: "Escola criada com sucesso!",
                 escola: escola.rows[0]
             })
-        }catch(erro){
+        } catch (erro) {
             console.error("Erro no endpoint de criar escola, erro: ", erro)
+            return res.status(500).json({
+                msg: "Erro Interno do Servidor."
+            })
+        }
+    },
+    editarEscola: async (req: Request, res: Response) => {
+        // pegando id do motorista do middleware
+        const motoristaId = req.userId
+        // dados esperados: id. dados opcionais: nome, endereço, telefone, hora abertura e hora fechamento, latitude e longitude
+        const dadosBrutos = EditarEscolaSchema.safeParse(req.body)
+
+        // validação dos dados
+        if (!dadosBrutos.success) {
+            return res.status(400).json({
+                msg: "Dados Inválidos para editar a escola.",
+                erro: dadosBrutos.error.format()
+            })
+        }
+        // desestruturação dos dados
+        const { id, nome, endereco, tel, hora_abertura, hora_fechamento, latitude, longitude } = dadosBrutos.data
+
+        // iniciando arrays para guardar os dados recebidos.
+        const campos: string[] = []
+        const valores: (string | number)[] = []
+
+        // checagem para verificar quais campos foram recebidos.
+        if (!id) {
+            return res.status(400).json({
+                msg: "id é necessario para editar uma escola"
+            })
+        }
+        if(nome){
+            campos.push(`nome = $${valores.length + 1}`)
+            valores.push(nome)
+        }
+        if(endereco){
+            campos.push(`endereco = $${valores.length + 1}`)
+            valores.push(endereco)
+        }
+        if(tel){
+            campos.push(`tel = $${valores.length + 1}`)
+            valores.push(tel)
+        }
+        if(hora_abertura){
+            campos.push(`hora_abertura = $${valores.length + 1}`)
+            valores.push(hora_abertura)
+        }
+        if(hora_fechamento){
+            campos.push(`hora_fechamento = $${valores.length + 1}`)
+            valores.push(hora_fechamento)
+        }
+        if(latitude){
+            campos.push(`latitude = $${valores.length + 1}`)
+            valores.push(latitude)
+        }
+        if(longitude){
+            campos.push(`longitude = $${valores.length + 1}`)
+            valores.push(longitude)
+        }
+        
+        // se nenhum campo for recebido, manda embora
+        if(campos.length < 1){
+            return res.status(400).json({
+                msg: "É necessario pelo menos um campo para realizar a edição."
+            })
+        }
+
+        // Okay, agr realizar a operação
+        try{
+            const query = `
+            UPDATE escola
+            SET ${campos.join(', ')}
+            WHERE id = $${valores.length + 1} AND motorista_id = $${valores.length + 2} 
+            `
+            valores.push(id)
+            valores.push(motoristaId)
+            await database.query(query, valores)
+
+            return res.status(200).json({
+                msg: "Escola editada com sucesso!"
+            })
+        }catch(erro){
+            console.error("Erro no endpoint de editar escola, erro: ", erro)
             return res.status(500).json({
                 msg: "Erro Interno do Servidor."
             })
--- server/src/routes/escola.route.ts
+++ server/src/routes/escola.route.ts
@@ -7,4 +7,7 @@ const router = Router()
 // rota para realizar a criação da escola.
 router.post('/criar', middlewareAutenticar, controllerEscola.criarEscola)
 
+// rota para editar uma escola
+router.patch('/editar', middlewareAutenticar, controllerEscola.editarEscola)
+
 export default router
