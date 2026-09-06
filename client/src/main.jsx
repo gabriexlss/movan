22d94ad50f374a5c56cd02e5b54929474546867a
vitordefreitas2707 <vitordefreitas2707@gmail.com>
2026-09-06 02:09:53 -0300

    fix/funcionamento do toast

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter } from 'react-router-dom';
 import { createRoot } from 'react-dom/client'
 import { GoogleOAuthProvider } from '@react-oauth/google'
+import { Toaster } from 'react-hot-toast'
 import App from './App.jsx'
 import { AuthProvider } from './context/AuthContext.jsx'
 
@@ -11,5 +12,6 @@ createRoot(document.getElementById('root')).render(
         <App />
       </AuthProvider>
     </GoogleOAuthProvider>
+    <Toaster position="top-right" />
   </BrowserRouter>,
 )
